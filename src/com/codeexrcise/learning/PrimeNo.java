1b738c86fa12cba6baa32d5d7388417029171fec
ItzMeMathews <mathewsmkg94@gmail.com>
2022-03-07 17:07:12 +0530

    Committing changes in PrimeNo file

--- src/com/codeexrcise/learning/PrimeNo.java
+++ src/com/codeexrcise/learning/PrimeNo.java
@@ -3,7 +3,7 @@ package com.codeexrcise.learning;
 public class PrimeNo {
 
 	public static void main(String[] args) {
-
+       // Test comment for pushing to git hub
 		for (int i = 2; i <= 100; i++) {
 			int count = 0;
 			for (int j = 1; j <= i; j++) {
